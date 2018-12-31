1bc4451f13a104aa9a9b2bcf379d04d3ce514f09
Azza-Ahmed <azza.h.mostafa@gmail.com>
2019-01-01 01:09:09 +0200

    docs: document 'Task' class

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 function Task(content){
   var self = this;
+  // text to hold task details
   self.taskContent = ko.observable(content);
+  // isDone reflects the state of the task (done or still undone)
   self.isDone = ko.observable(false);
+  // editing denotes whether it's view or edit mode
   self.editing = ko.observable(false);
-  // Behaviors
-  self.edit = function() { this.editing(true) }
 
+  // Behaviors
+
+  // set editing mode to true
+  self.edit = function() {
+    this.editing(true)
+  };
+
+  // toggle 'isDone' state
   self.toggleTaskState = function(){
     self.isDone(!self.isDone());
   };
